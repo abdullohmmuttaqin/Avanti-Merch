03b2c3566309501cf654f9ae94595463e18b96f4
Abd Muttaqin <abdullohmmuttaqin.09@gmail.com>
2026-01-25 04:30:26 +0700

    kembaliin webkit

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ document.addEventListener('alpine:init', () => {
 
     Alpine.data('products', () => ({
         items: [
-            { id: 1, name: 'TS NIRVANA', img: 'N.jpg', price: 650000 }
-    { id: 1, name: 'TS NECK DEEP', img: 'ND.jpg', price: 650000 }
-    { id: 1, name: 'TS GREEN DAY', img: 'GD.jpg', price: 650000 }
-    { id: 1, name: 'TS BLINK-182', img: 'B.jpg', price: 650000 }
-    { id: 1, name: 'TS SYSTEM OF A DOWN', img: 'SOAD.jpg', price: 650000 }
+            { id: 1, name: 'TS NIRVANA', img: 'N.jpg', price: 650000 },
+            { id: 1, name: 'TS NECK DEEP', img: 'ND.jpg', price: 650000 },
+            { id: 1, name: 'TS GREEN DAY', img: 'GD.jpg', price: 650000 },
+            { id: 1, name: 'TS BLINK-182', img: 'B.jpg', price: 650000 },
+            { id: 1, name: 'TS SYSTEM OF A DOWN', img: 'SOAD.jpg', price: 650000 },
         ],
     }));
 });
