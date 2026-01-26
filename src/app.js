f7714c9bc2af02e6177b8472c2d0e01059fca535
Abd Muttaqin <abdullohmmuttaqin.09@gmail.com>
2026-01-27 00:24:15 +0700

    menambahkan fitur kurangi di cart item pada index dan app.js

--- src/app.js
+++ src/app.js
@@ -41,6 +41,32 @@ document.addEventListener('alpine:init', () => {
 
             }
         },
+        remove(id) {
+            // ambil item yang mau di remove berdasarkan id nya
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // jika item lebih dari 1
+            if (cartItem.quantity > 1) {
+                // Telusuri 1 1
+                this.items = this.items.map((item) => {
+                    // jika bukan barang yang di klik
+                    if (item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.total = item.price * item.quantity;
+                        this.quantity--;
+                        this.total -= item.price;
+                        return item;
+                    }
+                })
+            } else if (cartItem.quantity === 1) {
+                // jika barang sisa 1
+                this.items = this.items.filter((item) => item.id !== id);
+                this.quantity--;
+                this.total -= cartItem.price
+            }
+        }
     });
 });
 
